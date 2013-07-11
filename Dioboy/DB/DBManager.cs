6c2a364392a6259f658e33bd0efe0bad0d1d8dda
vivatzx <hacked@empal.com>
2013-07-11 23:31:02 +0900

    로컬 DB 파일 추가 및 entity 객체 생성, DBManager Singleton으로 작성

--- Dioboy/DB/DBManager.cs
+++ Dioboy/DB/DBManager.cs
@@ -8,5 +8,30 @@ namespace Dioboy.DB
 {
 	class DBManager
 	{
+		private static volatile DBManager _Instance = null;
+		private static object _SyncRoot = new Object();
+
+		private DioboyDBEntities _DB = new DioboyDBEntities();
+
+		public static DBManager Instance
+		{
+			get
+			{
+				if (null == _Instance)
+				{
+					lock (_SyncRoot)
+					{
+						if (null == _Instance)
+						{
+							_Instance = new DBManager();
+						}
+					}
+				}
+
+				return _Instance;
+			}
+		}
+
+		private DBManager() { }
 	}
 }
